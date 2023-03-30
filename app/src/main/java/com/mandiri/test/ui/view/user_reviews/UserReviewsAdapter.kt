5a3f5e9ad68b85cbda22b3a639fa586acf318efb
ldileh <ldileh@gmail.com>
2023-03-30 23:11:57 +0700

    changelog: update items of user reviews

--- app/src/main/java/com/mandiri/test/ui/view/user_reviews/UserReviewsAdapter.kt
+++ app/src/main/java/com/mandiri/test/ui/view/user_reviews/UserReviewsAdapter.kt
@@ -3,9 +3,11 @@ package com.mandiri.test.ui.view.user_reviews
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.mandiri.core.utils.ext.dp
 import com.mandiri.core.utils.ext.safe
 import com.mandiri.test.databinding.ItemUserReviewBinding
 import com.mandiri.test.domain.remote.model.Review
@@ -15,17 +17,28 @@ class UserReviewsAdapter: PagingDataAdapter<Review, UserReviewsAdapter.ViewHolde
     inner class ViewHolder(private val binding: ItemUserReviewBinding) : RecyclerView.ViewHolder(binding.root){
 
         @SuppressLint("SetTextI18n")
-        fun bind(data: Review){
+        fun bind(position: Int, data: Review){
             binding.apply {
                 tvName.text = data.author.safe()
                 tvRating.text = "Rating: ${data.authorDetails?.rating.safe()}"
                 tvReview.text = data.content.safe()
+
+                val params = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+                )
+                when(position){
+                    0 -> params.setMargins(10.dp, 16.dp, 10.dp, 0.dp)
+                    else -> params.setMargins(10.dp, 0.dp, 10.dp, 0.dp)
+                }
+
+                root.layoutParams = params
             }
         }
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        getItem(position)?.let { holder.bind(it) }
+        getItem(position)?.let { holder.bind(position, it) }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
--- core/src/main/java/com/mandiri/core/utils/ext/CommonExt.kt
+++ core/src/main/java/com/mandiri/core/utils/ext/CommonExt.kt
@@ -1,5 +1,7 @@
 package com.mandiri.core.utils.ext
 
+import android.content.res.Resources
+
 fun Int?.safe() = this ?: 0
 
 fun String?.safe() = this ?: ""
@@ -7,3 +9,6 @@ fun String?.safe() = this ?: ""
 fun Double?.safe() = this ?: 0.0
 
 fun Long?.safe() = this ?: 0L
+
+val Int.dp: Int
+    get() = (this * Resources.getSystem().displayMetrics.density + 0.5f).toInt()
